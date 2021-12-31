742c17d30e73a7875a9731c23eae9c88cad77484
Jorge Marquez <jorgemarquez2222@gmail.com>
2021-12-31 00:04:13 -0300

    first commit

--- greet/english.go
+++ greet/english.go
@@ -1,6 +1,61 @@
 package greet
 
-// English returns a string "Hello, World"
+type course struct {
+	Name    string
+	Price   float64
+	IsFree  bool
+	UserIDs []int
+	Clases  map[uint]string
+}
+
+func (c *course) GetName() string {
+	return c.Name
+}
+
+func (c *course) GetPrice() float64 {
+	return c.Price
+}
+func (c *course) GetIsFree() bool {
+	return c.IsFree
+}
+
+func (c *course) GetUserIDs() []int {
+	return c.UserIDs
+}
+
+func (c *course) GetClases() map[uint]string {
+	return c.Clases
+}
+func (c *course) SetName(name string) {
+	c.Name = name
+}
+
+func (c *course) SetPrice(price float64) {
+	c.Price = price
+}
+
+func (c *course) SetIsFree(isFree bool) {
+	c.IsFree = isFree
+}
+
+func (c *course) SetUserIDs(userIDs []int) {
+	c.UserIDs = userIDs
+}
+
+func (c *course) SetClases(clases map[uint]string) {
+	c.Clases = clases
+}
+
+func NewCourse(name string, price float64, isFree bool, userIDs []int, clases map[uint]string) *course {
+	return &course{
+		Name:    name,
+		Price:   price,
+		IsFree:  isFree,
+		UserIDs: userIDs,
+		Clases:  clases,
+	}
+}
+
 func English() string {
-	return "Hola en ingles"
+	return "Hello"
 }
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/jorgemarquez2222/test/greet"
+	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Print("Hello, World")
-
+	fmt.Print(greet.English())
+	fmt.Println(quote.Hello())
 }
